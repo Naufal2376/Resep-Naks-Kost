7cf0b0a3c25876327d8f580fb815a0b949e1259e
Naufal <naufalbhurr@gmail.com>
2025-02-26 22:28:33 +0700

    update

--- src/components/CardAnimate.tsx
+++ src/components/CardAnimate.tsx
@@ -4,27 +4,27 @@ import { Marquee } from "@/components/magicui/marquee"
 const reviews = [
   {
     id: 1,
-    name: "Sarden Kaleng ABC",
-    body: "Sarden ABC selalu jadi penyelamat akhir bulan! Cepat, enak, dan bergizi.",
+    name: "Sarden Kaleng Rasa Janji Manis",
+    body: "Dibuka penuh harapan, aromanya menggoda, rasanya manis di awal. Tapi lama-lama sadar, isinya nggak sebanyak yang dibayangkan.",
     img: "/asset/Resep-Sarden-ABC.jpg",
   },
   {
     id: 2,
-    name: "Mie Instan Pedas",
-    body: "Mie instan ini punya rasa pedas yang bikin ketagihan. Cocok buat anak kost!",
-    img: "/asset/Mie-Instan-Pedas.jpg",
+    name: "Mie Goreng CLBK (Cuma Laper Bukan Kangen)",
+    body: "Dikira nostalgia, padahal cuma butuh sesuatu yang gampang dimasak! Mie goreng ini solusi buat perut yang kosong, bukan hati yang kosong.",
+    img: "/asset/mie-clbk.jpg",
   },
   {
     id: 3,
-    name: "Nasi Goreng Kecap",
-    body: "Resep simpel tapi nikmat. Hanya butuh nasi, kecap, dan telur!",
-    img: "/asset/Nasi-Goreng-Kecap.jpg",
+    name: "Nasi Goreng Squad",
+    body: "Selalu siap menemani di saat lapar melanda, sederhana tapi penuh rasa. Tapi meski sering diandalkan, tetap aja dianggap biasa. yaa ini pilihan yang tepat buat anak kost yang gak mau ribet.",
+    img: "/asset/nasi-goreng-squad.jpg",
   },
   {
     id: 4,
-    name: "Telur Dadar Crispy",
-    body: "Telur dadar ini beda dari biasanya, ada sensasi crispy yang mantap!",
-    img: "/asset/Telur-Dadar-Crispy.jpg",
+    name: "Kentang Goreng Pedas Ditampar Kenyataan",
+    body: "Gurihnya bikin jatuh cinta di gigitan pertama, tapi pedasnya mulai terasa setelah beberapa suapan. Awalnya dinikmati, lama-lama bikin kepedesan sendiri. Yaa ini hanya tentang kentang.",
+    img: "/asset/kentang-mustofa.jpg",
   },
 ]
 
@@ -42,9 +42,9 @@ const ReviewCard = ({
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-6 flex flex-col items-center gap-4",
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+        "relative w-80 cursor-pointer overflow-hidden rounded-xl border p-6 flex flex-col items-center gap-4",
+        "border-gray-950/[.1] bg-[var(--secondary] hover:bg-[var(--secondary]/80",
+        "dark:border-gray-50/[.1] dark:bg-[var(--secondary] dark:hover:bg-[var(--secondary]/80"
       )}
     >
       <img
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <section
-      className="relative bg-cover bg-center h-[80vh] flex items-center justify-center text-center mx-14 my-10 rounded-3xl overflow-hidden"
+      className="relative bg-cover bg-center h-[80vh] flex items-center justify-center text-center mx-14 mt-24 rounded-3xl overflow-hidden"
       style={{ backgroundImage: "url('/asset/hero.jpg')" }}
     >
       {/* Overlay Gelap */}
-      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="absolute inset-0 bg-black/70"></div>
 
       {/* Konten Hero */}
       <div className="relative z-10 p-6">
@@ -13,12 +13,13 @@ export default function Hero() {
           NOODLE LOVERS <br /> AND SIMPLE EATS
         </h1>
         <p className="text-lg text-gray-200 mt-4 max-w-2xl mx-auto">
-          Jelajahi dunia rasa, temukan resep praktis nan lezat, dan biarkan
-          aroma semangat memasak kami menghampiri dapurmu.🍜
+          Jelajahi dunia rasa, temukan resep praktis nan lezat,
+          <br /> dan biarkan aroma semangat memasak kami <br /> menghampiri
+          dapurmu.🍜
         </p>
         <a
           href="#recipes"
-          className="mt-6 inline-block bg-[var(--primary)] text-black font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-opacity-80 transition"
+          className="mt-10 inline-block bg-[var(--primary)] text-black font-semibold px-6 py-3 rounded-3xl shadow-md hover:bg-opacity-80 transition"
         >
           Jelajahi Resep
         </a>
